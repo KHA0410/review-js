11bf729f2cd084d7ce499ddc7e3b5d0bc3eae84b
KHA0410 <khanguyen151121@gmail.com>
2025-03-23 23:50:23 +0700

    review array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,4 @@
-console.log('message')
+var languages = ["JS", "C++", "C#", 123, undefined, function(){}, null];
+var a = 3;
+console.log('languages', languages.unshift("vn"))
+console.log('languages', languages)
