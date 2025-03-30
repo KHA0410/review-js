ce478a3f7514bf5e4f8d9e0f3e8e9dd7d696dd21
KHA0410 <khanguyen151121@gmail.com>
2025-03-30 10:32:23 +0700

    about string

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,7 @@ var a = 3;
 console.log('languages', languages.unshift("vn"))
 console.log('languages', languages)
 
+var lastName = "DANG";
+var firstName = "SON";
+console.log('name: ', `${"Tôi tên: " + firstName}` )
+console.log(lastName.indexOf("G"))
